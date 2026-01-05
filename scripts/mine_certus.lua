27c17bf129b37da7be243ced439b5cac8ea23b94
Datacraft <frood.datacraft@gmail.com>
2026-01-05 01:01:11 -0800

    Update mine certus script

--- scripts/mine_certus.lua
+++ scripts/mine_certus.lua
@@ -2,11 +2,13 @@ while true do
     a, b = turtle.inspect()
     if b.name == "ae2:quartz_cluster" then
         turtle.dig()
-        for i=1,16 do
-            turtle.select(i)
-            turtle.dropUp()
-        end
     end
+    a, b = turtle.inspectDown()
+    if b.name == "ae2:quartz_cluster" then
+        turtle.digDown()
+    end
+    turtle.select(1)
+    turtle.dropUp()
     os.sleep(1)
 end
 
